967fee22250069a03eea5224448755d16584c801
Аrtem Sorokin 153503 <93012013+Axel7509@users.noreply.github.com>
2023-02-22 01:37:08 +0300

    done second point

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,2 +1,20 @@
+def my_function(num1, num2, op):
+    if op == "add":
+        return num1 + num2
+    elif op == "sub":
+        return num1 - num2
+    elif op == "mult":
+        return num1 * num2
+    elif op == "div":
+        return num1 / num2
+    else:
+        return "incorrect operation"
+
 
 print("Hello world")
+
+number1 = int(input("input num 1 : "))
+number2 = int(input("input num 2 : "))
+operation = input("input operation : ")
+
+print(my_function(number1, number2, operation))
