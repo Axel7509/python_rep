da271bcc35a895f8852c3160d69437042100f558
Аrtem Sorokin 153503 <93012013+Axel7509@users.noreply.github.com>
2023-02-22 13:35:03 +0300

    edited files

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,23 +1,15 @@
-def my_function(num1, num2, op):
-    if op == "add":
-        return num1 + num2
-    elif op == "sub":
-        return num1 - num2
-    elif op == "mult":
-        return num1 * num2
-    elif op == "div":
-        return num1 / num2
-    else:
-        return "incorrect operation"
-
+from calculator import my_function
 
 print("Hello world")
 
-number1 = int(input("input num 1 : "))
-number2 = int(input("input num 2 : "))
-operation = input("input operation : ")
+try:
+    number1 = int(input("input num 1 : "))
+    number2 = int(input("input num 2 : "))
+    operation = input("input operation : ")
 
-print(my_function(number1, number2, operation))
+    print(my_function(number1, number2, operation))
+except Exception as e:
+    print(e.args)
 
 numList = list(map(int, input().split()))
 evenList = [i for i in numList if i % 2 == 0]
