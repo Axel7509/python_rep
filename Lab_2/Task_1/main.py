dd6e025e437c772698081cd1f080fb8f0f4653a6
Аrtem Sorokin 153503 <93012013+Axel7509@users.noreply.github.com>
2023-03-18 17:16:15 +0300

    add average length if the sentence

--- Lab_2/Task_1/main.py
+++ Lab_2/Task_1/main.py
@@ -7,10 +7,13 @@ def main():
     file = open('public/text.txt', encoding='utf-8')
     text = file.read()
     print(text)
+
     print("\n", "#"*30, "\n")
-    print(f"Average word length in text = {words.average_word_length(text)}")
+
     print(f"Numbers of sentences in the text = {sentences.count_sentences(text)}")
     print(f"Numbers of non-declarative sentences in the text = {sentences.count_non_declarative(text)}")
+    print(f"Average length of the word in the text = {words.average_word_length(text)}")
+    print(f"Average length of the sentence = {words.average_sentence_length(text)}")
 
 
 if __name__ == '__main__':
--- Lab_2/Task_1/utils/words.py
+++ Lab_2/Task_1/utils/words.py
@@ -2,6 +2,7 @@
 import re
 from utils.helpers import get_words
 from utils.helpers import count_characters
+from utils.sentences import count_sentences
 
 
 def average_word_length(text: str) -> float:
@@ -11,3 +12,8 @@ def average_word_length(text: str) -> float:
 
     return characters / len(words)
 
+
+def average_sentence_length(text: str) -> float:
+    """Counts average sentence-length in the text"""
+
+    return len(get_words(text)) / count_sentences(text)
