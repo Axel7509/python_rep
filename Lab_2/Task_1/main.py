c928b6285099524f2850b02646d0222acf838610
Аrtem Sorokin 153503 <93012013+Axel7509@users.noreply.github.com>
2023-03-19 16:21:45 +0300

    remake add top k_n_grams

--- Lab_2/Task_1/main.py
+++ Lab_2/Task_1/main.py
@@ -1,6 +1,5 @@
-import utils.helpers as help
-from utils import words, sentences
 
+from utils import words, sentences
 
 
 def main():
@@ -9,13 +8,15 @@ def main():
     print(text)
 
     print("\n", "#"*30, "\n")
+    n: int = 4
+    k: int = 10
 
     print(f"Numbers of sentences in the text = {sentences.count_sentences(text)}")
     print(f"Numbers of non-declarative sentences in the text = {sentences.count_non_declarative(text)}")
     print(f"Average length of the word in the text = {words.average_word_length(text)}")
     print(f"Average length of the sentence = {words.average_sentence_length(text)}")
+    print(f"Top {k} repeated {n}-grams are",f"{words.top_k_n_grams(text, n, k)}")
 
 
 if __name__ == '__main__':
     main()
-
--- Lab_2/Task_1/utils/words.py
+++ Lab_2/Task_1/utils/words.py
@@ -17,3 +17,17 @@ def average_sentence_length(text: str) -> float:
     """Counts average sentence-length in the text"""
 
     return len(get_words(text)) / count_sentences(text)
+
+
+def top_k_n_grams(text: str, n: int = 4, k: int = 10) -> list[str]:
+    """Returns top-K repeated N-grams in the text"""
+
+    text = re.sub(r"[!?.,;:-]", '', text)
+    words = get_words(text)
+    n_grams = tuple(
+        " ".join(words[i:i+n])
+        for i in range(len(words)) if i + n <= len(words)
+    )
+    unique_n_grams = sorted(set(n_grams), key=n_grams.count, reverse=True)
+
+    return unique_n_grams[:k]
