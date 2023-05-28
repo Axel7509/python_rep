0aa19ff7a7b74c22d2e3bcb2014de8e2fd745a26
Arterg <sorokin-arter@yandex.ru>
2023-05-29 00:31:50 +0300

    add dump and load

--- Lab_3/main.py
+++ Lab_3/main.py
@@ -1,6 +1,8 @@
+"""Main module of Lab_3"""
+
+
 def main():
     """Carries out the subtasks of Lab_3"""
-    pass
 
 
 if __name__ == '__main__':
--- Lab_3/utils/serialization.py
+++ Lab_3/utils/serialization.py
@@ -1,15 +1,56 @@
-
-def dump(self, obj: object, fp: str):
-    pass
-
-
-def dumps(self, obj: object):
-    pass
+import types
+from abc import ABC, abstractmethod
+from types import NoneType
+from typing import Callable, Any, IO
 
 
-def load(self, fp: str):
-    pass
+class Serializer(ABC):
+    _BASE_TYPES = [int, float, complex, bool, str, NoneType]
+
+    def __init__(self):
+        pass
+    @abstractmethod
+    def dump(self, obj, fp):
+        pass
+    @abstractmethod
+    def dumps(self, obj):
+        pass
+    @abstractmethod
+    def load(self, fp):
+        pass
+    @abstractmethod
+    def loads(self, s):
+        pass
 
 
-def loads(self, s: str):
-    pass
+class JSONSerializer(Serializer):
+    def __init__(self):
+        super().__init__()
+
+    def dump(self, obj: Any, fp: IO[str]):
+        if type(obj) in self._BASE_TYPES:
+            if type(obj) == str:
+                fp.write(f'"{obj}"')
+            else:
+                fp.write(str(obj))
+
+    def dumps(self, obj: Any) -> str:
+        if type(obj) in self._BASE_TYPES:
+            return f"{repr(obj)}"
+
+    def load(self, fp: IO[str]):
+        fp_content: str = fp.read()
+        return self.loads(fp_content)
+
+    def loads(self, s: str) -> Any:
+        if not len(s):
+            return None
+
+        if s.startswith("'") or s.startswith('"'):
+            return s.strip("'\"")
+
+        for t in self._BASE_TYPES:
+            try:
+                return t(s) if t != bool else (True, False)[s == "False"]
+            except ValueError:
+                pass
