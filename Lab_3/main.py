06084ffb07abb580b406b1f2d45a912ed8a74c71
Arterg <sorokin-arter@yandex.ru>
2023-05-29 21:14:57 +0300

    Add function serialization & json templates

--- Lab_3/main.py
+++ Lab_3/main.py
@@ -1,8 +1,23 @@
-"""Main module of Lab_3"""
+from utils.serialization import JSONSerializer
+
+
+def aboba(a: int = 1, b: int = 2) -> int:
+    c = 11
+    return c * (a + b)
 
 
 def main():
     """Carries out the subtasks of Lab_3"""
+    json = JSONSerializer()
+    print("_" * 20)
+    ahuha = json.loads(json.dumps(aboba))
+    print("_"*20)
+    print(dir(ahuha.__code__) == dir(aboba.__code__))
+    print(aboba.__code__.co_code == ahuha.__code__.co_code)
+    print(aboba.__code__.co_code)
+    print(ahuha.__code__.co_code)
+
+    # print(json._process_dict(json._parse_dictlike(json.dumps(aboba))))
 
 
 if __name__ == '__main__':
